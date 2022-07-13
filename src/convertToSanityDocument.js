926e56c81da15553ef0fea1725087c7c52068fa3
ysrji <45195460+ysrji@users.noreply.github.com>
2022-07-13 15:09:04 +0800

    wip

--- src/convertToSanityDocument.js
+++ src/convertToSanityDocument.js
@@ -1,13 +1,24 @@
 const convertHTMLtoPortableText = require('./convertHTMLtoPortableText')
 const {format} = require('date-fns')
 function convertToSanityDocument({data = {}, contents}) {
-  const { title, date } = data.frontmatter || {}
+  const { _type, tags, title, date, image, slug } = data.frontmatter || {}
   const portableText = convertHTMLtoPortableText(contents)
 
   const doc = {
-    _type: 'post',
+    // _type: 'post',
+    _type,
     _createdAt: format(new Date(date)),
-    publishedAt: format(new Date(date)),
+    // date: format(new Date(date)),
+    date,
+    image: {
+      "_type": "image",
+      "_sanityAsset": `image@file://.${image}`,
+    },
+    slug: {
+      "_type": "slug",
+      "current": slug,
+    },
+    tags,
     title,
     body: portableText
   }
