5ae1304f817a016e265ea0d723999001559951df
ysrji <45195460+ysrji@users.noreply.github.com>
2022-07-22 14:15:34 +0800

    now including random UUID as _id as well as all previously hidden posts

--- src/convertToSanityDocument.js
+++ src/convertToSanityDocument.js
@@ -1,15 +1,18 @@
+const { randomUUID } = require('crypto')
 const convertHTMLtoPortableText = require('./convertHTMLtoPortableText')
 const {format} = require('date-fns')
 function convertToSanityDocument({data = {}, contents}) {
-  const { _type, tags, title, date, image, slug } = data.frontmatter || {}
+  const { _id, _type, tags, title, date, image, slug, hidden } = data.frontmatter || {}
   const portableText = convertHTMLtoPortableText(contents)
 
   const doc = {
     // _type: 'post',
+    _id: randomUUID(),
     _type,
     _createdAt: format(new Date(date)),
     // date: format(new Date(date)),
     date,
+    hidden,
     image: {
       "_type": "image",
       "_sanityAsset": `image@file://.${image}`,
